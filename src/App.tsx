862e1e9933d4cff94ee7953f52eb426d7ab04fce
jl917 <julong1988@naver.com>
2024-11-09 22:21:57 +0900

    feat: 플러그인 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { ask } from "@tauri-apps/plugin-dialog";
 import "./App.css";
 
 function App() {
@@ -14,17 +15,38 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>Welcome to Tauri + React333</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+        >
+          <img
+            src="/vite.svg"
+            className="logo vite"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        <a
+          href="https://tauri.app"
+          target="_blank"
+        >
+          <img
+            src="/tauri.svg"
+            className="logo tauri"
+            alt="Tauri logo"
+          />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
@@ -43,6 +65,18 @@ function App() {
         />
         <button type="submit">Greet</button>
       </form>
+      <button
+        onClick={async () => {
+          const answer = await ask("This action cannot be reverted. Are you sure?", {
+            title: "Tauri",
+            kind: "warning",
+          }).then((res) => {
+            console.log(res);
+          });
+        }}
+      >
+        dialog
+      </button>
       <p>{greetMsg}</p>
     </main>
   );
