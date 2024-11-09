db456ad4c5f4adf47cadd187dd682af62f32e3a6
jl917 <julong1988@naver.com>
2024-11-09 22:57:34 +0900

    fix: 권한 허용

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React333</h1>
+      <h1>Welcome to Tauri + React</h1>
 
       <div className="row">
         <a
